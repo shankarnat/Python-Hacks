593c3dd428ff805718bbc5115bfbe99b5f9f5395
Shankar <shankan@microsoft.com>
2017-12-06 11:07:03 +0530

    List Current Directory Files

---
+++ ListCurDir.py
@@ -0,0 +1,8 @@
+#List Files in a the current Directory
+import os
+
+list_files = os.listdir()
+for filenam in list_files:
+    print(filenam)
+
+#This shows the hidden files too; we need to figure out how to hide them
