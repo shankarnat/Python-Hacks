1a7cddef55d9d43f2d1d75e0d34a945af2f4c682
Nikita Shankar <nikitashankar@Nikitas-iMac.local>
2024-04-17 23:21:22 +0530

    List Comprehension

--- comprehension.py
+++ comprehension.py
@@ -1,9 +1,13 @@
 #Implement Dict Comprehension
 d = {n: n**2 for n in range(5)}
 print(d)
+
+#Implementing List Comprehension
+print([i for i in range(0,3)])
+
 l = ["sh","an","kar"]
 
 
 s = "".join(l)
 
-print(type(s))
+print(s)
